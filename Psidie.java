5f1e4d0d35b9e50306f852e79a4375a08044fe0a
Erick Sanchez <sanchez8erick@gmail.com>
2023-09-11 17:51:07 -0400

    Renaming

--- Psidie.java
+++ Psidie.java
@@ -4,236 +4,254 @@
 // import org.jfree.chart.plot.PlotOrientation;
 // import org.jfree.data.statistics.HistogramDataset;
 // import org.jfree.data.statistics.HistogramType;
+
 // import java.io.File;
 // import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
 
+// import static java.lang.Math.round;
+
 public class Psidie {
-    public static int psidieOriginal = 12;
     static int result = 0;
-    static int amount = 0;
-    static int psidie = psidieOriginal;
+    static int numOfRolls = 0;
+    static int numOfd12 = 0;
+    static int numOfd10 = 0;
+    static int numOfd8 = 0;
+    static int numOfd6 = 0;
+    static int numOfd4 = 0;
+    static int numOfd2 = 0;
+    static int attemptedTotal = 500000;
     static int pointsMade = 0;
-    static double total = 0;
-    static double average;
-    static double median;
-    static int attemptedTotal = 10000;
+    static double totalRolls= 0;
+    static double totald12 = 0;
+    static double totald10 = 0;
+    static double totald8 = 0;
+    static double totald6 = 0;
+    static double totald4 = 0;
+    static double totald2 = 0;
     static double lowest = 0;
     static double highest = 0;
-    static ArrayList<Double> rollsList = new ArrayList<>();
-    static ArrayList<Double> pointList = new ArrayList<>();
+    static ArrayList<Integer> rollsList = new ArrayList<>();
+    static ArrayList<Integer> pointList = new ArrayList<>();
+    static ArrayList<Integer> d12List = new ArrayList<>();
+    static ArrayList<Integer> d10List = new ArrayList<>();
+    static ArrayList<Integer> d8List = new ArrayList<>();
+    static ArrayList<Integer> d6List = new ArrayList<>();
+    static ArrayList<Integer> d4List = new ArrayList<>();
+    static ArrayList<Integer> d2List = new ArrayList<>();
     static double totalPoints = 0;
-    static double averagePoints = 0;
     static Random myRand = new Random();
+    public static int psidieOriginal = 6;
+    static int psidie = psidieOriginal;
+    static boolean include_d12 = false;
+    static boolean include_d10 = false;
+    static boolean include_d8 = false;
+    static boolean include_d6 = true;
+    static boolean include_d4 = true;
+    static boolean include_d2 = true;
+
 
     public static void main(String[] args) {
+        testDice();
+
+        Collections.sort(rollsList);
+        trimList(rollsList, 5);
+        System.out.println("------Rolls List------");
+        printListInfo(rollsList);
+
+//        Collections.sort(pointList);
+//        trimList(pointList, 5);
+//        System.out.println("------Points List------");
+//        printListInfo(pointList);
+
+        if(include_d2){
+            Collections.sort(d2List);
+            trimList(d2List, 5);
+            System.out.println("------d2 List------");
+            printListInfoDice(d2List);
+        }
+        if(include_d4){
+            Collections.sort(d4List);
+            trimList(d4List, 5);
+            System.out.println("------d4 List------");
+            printListInfoDice(d4List);
+        }
+        if(include_d6){
+            Collections.sort(d6List);
+            trimList(d6List, 5);
+            System.out.println("------d6 List------");
+            printListInfoDice(d6List);
+        }
+        if(include_d8){
+            Collections.sort(d8List);
+            trimList(d8List, 5);
+            System.out.println("------d8 List------");
+            printListInfoDice(d8List);
+        }
+        if(include_d10){
+            Collections.sort(d10List);
+            trimList(d10List, 5);
+            System.out.println("------d10 List------");
+            printListInfoDice(d10List);
+        }
+        if(include_d12){
+            Collections.sort(d12List);
+            trimList(d12List, 5);
+            System.out.println("------d12 List------");
+            printListInfoDice(d12List);
+        }
 
 
+
+        // createRollChart();
+    }
+
+    public static void testDice(){
         for (int i = 0; i < attemptedTotal; i++) {
             psidie = psidieOriginal;
             do {
                 switch (psidie) {
                     case 12 -> {
-                        result = myRand.nextInt(psidie) + 1;
-                        pointsMade += result;
-                        totalPoints += result;
-                        amount++;
-                        if (result == 6) psidie = 10;
-                    }
-                    case 10 -> {{
+                        {
                             result = myRand.nextInt(psidie) + 1;
                             pointsMade += result;
-                            totalPoints += result;
-                            amount++;
-                            if (result == 5) psidie = 8;
+                            numOfRolls++;
+                            numOfd12++;
                         }
-                        if (result == 1) psidie += 2;
+                        if (result == 6) psidie -= 2;
                     }
-                    case 8 -> {{
+                    case 10 -> {
+                        {
                             result = myRand.nextInt(psidie) + 1;
                             pointsMade += result;
-                            totalPoints += result;
-                            amount++;
-                            if (result == 4) psidie -= 2;
+                            numOfRolls++;
+                            numOfd10++;
                         }
-                        if (result == 1) psidie += 2;
+                        if (result == 5 && include_d8) psidie -= 2;
+                        if (result == 1 && include_d12) psidie += 2;
                     }
-                    case 6 -> {{
+                    case 8 -> {
+                        {
                             result = myRand.nextInt(psidie) + 1;
                             pointsMade += result;
-                            totalPoints += result;
-                            amount++;
-                            if (result == 3) psidie -= 2;
+                            numOfRolls++;
+                            numOfd8++;
                         }
-                        if (result == 1) psidie += 2;
+                        if (result == 4 && include_d6) psidie -= 2;
+                        if (result == 1 && include_d10) psidie += 2;
                     }
-                    case 4 -> {{
+                    case 6 -> {
+                        {
                             result = myRand.nextInt(psidie) + 1;
                             pointsMade += result;
-                            totalPoints += result;
-                            amount++;
-                            if (result == 2) psidie = 0;
+                            numOfRolls++;
+                            numOfd6++;
                         }
-                        if (result == 1) psidie += 2;
+                        if (result == 3 && include_d4) psidie -= 2;
+                        if (result == 1 && include_d8) psidie += 2;
                     }
-                    case 2 -> {{
+                    case 4 -> {
+                        {
                             result = myRand.nextInt(psidie) + 1;
                             pointsMade += result;
-                            totalPoints += result;
-                            amount++;
-                            if (result == 2) psidie = 0;
+                            numOfRolls++;
+                            numOfd4++;
                         }
-                        if (result == 1) psidie += 2;
+                        if (result == 2 && include_d2) psidie = 2;
+                        else if (result == 2) psidie = 0;
+                        if (result == 1 && include_d6) psidie += 2;
+                    }
+                    case 2 -> {
+                        {
+                            result = myRand.nextInt(psidie) + 1;
+                            pointsMade += result;
+                            numOfRolls++;
+                            numOfd2++;
+                        }
+                        if (result == 2) psidie = 0;
+                        if (result == 1 && include_d4) psidie += 2;
                     }
                     default -> System.out.println("Error " + i);
                 }
             } while (psidie > 0);
-            rollsList.add((double)amount);
-            pointList.add((double)pointsMade);
+            rollsList.add(numOfRolls);
+            pointList.add(pointsMade);
+            d12List.add(numOfd12);
+            d10List.add(numOfd10);
+            d8List.add(numOfd8);
+            d6List.add(numOfd6);
+            d4List.add(numOfd4);
+            d2List.add(numOfd2);
             if (i == 0){
-                lowest = amount;
-                highest = amount;
+                lowest = numOfRolls;
+                highest = numOfRolls;
             }
-            if (amount < lowest) lowest = amount;
-            if (amount > highest) highest = amount;
-            total += amount;
-            amount = 0;
+            if (numOfRolls < lowest) lowest = numOfRolls;
+            if (numOfRolls > highest) highest = numOfRolls;
+            totalRolls += numOfRolls;
+            totalPoints += pointsMade;
+            totald12 += numOfd12;
+            totald10 += numOfd10;
+            totald8 += numOfd8;
+            totald6 += numOfd6;
+            totald4 += numOfd4;
+            totald2 += numOfd2;
+            numOfRolls = 0;
             pointsMade = 0;
+            numOfd12 = 0;
+            numOfd10 = 0;
+            numOfd8 = 0;
+            numOfd6 = 0;
+            numOfd4 = 0;
+            numOfd2 = 0;
         }
-        average = total /(double)attemptedTotal;
-        averagePoints = totalPoints / (double)attemptedTotal;
-
-        Collections.sort(rollsList);
-        Collections.sort(pointList);
-        //System.out.println(pointList);
-        System.out.println("Average: " + average);
-        rollMedian();
-        System.out.println("Highest: " + highest);
-        System.out.println("Lowest: " + lowest);
-        System.out.println("Total Points on average: " + averagePoints);
-
-        // remove 25% of top and bottom and find mean
-        trimAverageRolls();
-
-        // remove 25% of top and bottom and find median
-        trimMedianRolls();
-
-        // remove 25% of top and bottom and find mean for result
-        trimAveragePoints();
-
-        // trim 25% of top and bottom to remove outliers and find median for pointsList
-        trimMedianPoints();
-
-        //create chart for rolls
-        // createRollChart();
-
-        //create chart for points
-        // createPointChart();
     }
-
-    private static void trimMedianPoints() {
-        int pointListSizeOrig = pointList.size();
-        //Trims
-        for(int i = 0; i < pointListSizeOrig*0.25; i++){
-            //rollsList.remove(0);
-            pointList.remove(pointList.size()-1);
+    public static void trimList(ArrayList<Integer> list, double trimPercent){
+        int trim = (int) (list.size() * (trimPercent / 100));
+        for (int i = 0; i < trim; i++) {
+            list.remove(0);
+            list.remove(list.size() - 1);
         }
-
-        totalPoints = 0;
-        highest = 0;
-        lowest = 100;
-        for (Double aDouble : pointList) {
-            totalPoints = totalPoints + aDouble;
-
-            if (aDouble < lowest)
-                lowest = aDouble;
-            if (aDouble > highest)
-                highest = aDouble;
-        }
-        System.out.println("Points Trimmed Median: " + averagePoints);
     }
-
-    private static void trimMedianRolls() {
-        int rollsListSizeOrig = rollsList.size();
-        //Trims
-        for(int i = 0; i < rollsListSizeOrig*0.25; i++){
-            //rollsList.remove(0);
-            rollsList.remove(rollsList.size()-1);
+    public static int getListMedian(ArrayList<Integer> list){
+        if (list.size() % 2 == 0){
+            return (list.get(list.size() / 2) + list.get((list.size() / 2) - 1)) / 2;
+        } else {
+            return list.get(list.size() / 2);
         }
-
-        //find the median of rollsList
-        if (rollsList.size() % 2 == 0)
-            median = ((double)rollsList.get(rollsList.size()/2) + (double)rollsList.get(rollsList.size()/2 - 1))/2;
-        else
-            median = (double) rollsList.get(rollsList.size()/2);
-
-        System.out.println("Rolls Trimmed Median: " + median);
     }
-
-    private static void rollMedian() {
-        if (rollsList.size() % 2 == 0)
-            median = ((double)rollsList.get(rollsList.size()/2) + (double)rollsList.get(rollsList.size()/2 - 1))/2;
-        else
-            median = (double) rollsList.get(rollsList.size()/2);
-        System.out.println("Median: " + median);
+    public static double getListTotal(ArrayList<Integer> list) {
+        double total = 0;
+        for (double d : list) {
+            total += d;
+        }
+        return total;
     }
-
-    public static void trimAverageRolls(){
-        int rollsListSizeOrig = rollsList.size();
-        //Trims
-        for(int i = 0; i < rollsListSizeOrig*0.25; i++){
-            //rollsList.remove(0);
-            rollsList.remove(rollsList.size()-1);
-        }
-
-        total = 0;
-        highest = 0;
-        lowest = 100;
-        for(int i = 0; i < rollsList.size(); i++){
-            total = total + rollsList.get(i);
-
-            if (rollsList.get(i) < lowest)
-                lowest = rollsList.get(i);
-            if (rollsList.get(i) > highest)
-                highest = rollsList.get(i);
-        }
-
-        average = (double) total / rollsList.size();
-
-        System.out.println("Rolls Trimmed Average: " + average);
-        System.out.println("Rolls Trimmed Highest: " + highest);
-        System.out.println("Rolls Trimmed Lowest: " + lowest);
+    public static double getListAverage(ArrayList<Integer> list) {
+        return getListTotal(list) / list.size();
     }
-
-    public static void trimAveragePoints(){
-        int pointListSizeOrig = pointList.size();
-        //Trims
-        for(int i = 0; i < pointListSizeOrig*0.25; i++){
-            //pointList.remove(0);
-            pointList.remove(pointList.size()-1);
-        }
-
-        total = 0;
-        highest = 0;
-        lowest = 100;
-        for(int i = 0; i < pointList.size(); i++){
-            total = total + pointList.get(i);
-
-            if (pointList.get(i) < lowest)
-                lowest = pointList.get(i);
-            if (pointList.get(i) > highest)
-                highest = pointList.get(i);
-        }
-
-        average = (double) total / pointList.size();
-
-        System.out.println("Points Trimmed Average: " + average);
-        System.out.println("Points Trimmed Highest: " + highest);
-        System.out.println("Points Trimmed Lowest: " + lowest);
+    public static void printListInfo(ArrayList<Integer> list){
+        System.out.println("Lowest: " + list.get(0));
+        System.out.println("Highest: " + list.get(list.size() - 1));
+        System.out.println("Median: " + getListMedian(list));
+        System.out.println("Average: " + round(getListAverage(list),2));
     }
+    public static void printListInfoDice(ArrayList<Integer> list){
+        System.out.println("Percentage: " + round((double)getListAverage(list)/getListAverage(rollsList)*100,2) + "%");
+        System.out.println("Median: " + getListMedian(list));
+        System.out.println("Average: " + round(getListAverage(list),2));
+    }
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
 
+        BigDecimal bd = BigDecimal.valueOf(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
     // public static void createRollChart(){
     //     double[] valuesRolls = new double[rollsList.size()];
     //     for (int i = 0; i < rollsList.size(); i++) {valuesRolls[i] = rollsList.get(i);}
@@ -244,22 +262,8 @@ public class Psidie {
 
     //     JFreeChart histogram = ChartFactory.createHistogram("Psidie","Rolls","Frequency",dataset, PlotOrientation.VERTICAL,false,false,false);
     //     try {
-
-    //         ChartUtils.saveChartAsPNG(new File("C://Users//Erick Sanchez//Desktop//amountOfRolls.png"), histogram, 600, 400);
-    //     }catch(IOException e) {}
+    //         ChartUtils.saveChartAsPNG(new File("C://Users//Erick Sanchez//Desktop//amountOfRolls.jpg"), histogram, 600, 400);
+    //     }catch(IOException ignored) {}
     // }
 
-    // public static void createPointChart(){
-    //     double[] valuesPoints = new double[pointList.size()];
-    //     for (int i = 0; i < pointList.size(); i++) {valuesPoints[i] = pointList.get(i);}
-    //     int binny = (int)Math.round(Collections.max(pointList));
-    //     HistogramDataset dataset = new HistogramDataset();
-    //     dataset.setType(HistogramType.RELATIVE_FREQUENCY);
-    //     dataset.addSeries("key", valuesPoints, binny);
-
-    //     JFreeChart histogram = ChartFactory.createHistogram("Psidie","Points","Frequency",dataset, PlotOrientation.VERTICAL,false,false,false);
-    //     try {
-    //         ChartUtils.saveChartAsPNG(new File("C://Users//Erick Sanchez//Desktop//amountOfPoints.png"), histogram, 600, 400);
-    //     }catch(IOException e) {}
-    // }
 }
