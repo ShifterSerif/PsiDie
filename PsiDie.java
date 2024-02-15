717c40e51ebfd7fb4da8c788bdfeacaa268b3a4c
Erick Sanchez <sanchez8erick@gmail.com>
2024-02-15 04:59:30 -0500

    added documentation to PsiDie.java

--- PsiDie.java
+++ PsiDie.java
@@ -1,6 +1,4 @@
 import java.util.Random;
-// find out if there is a way to make an array or something that makes it so you can easily add new dice to the program
-// just a one stop shop that makes new arrays, maybe booleans
 // check out Cobertura to see how many steps for optimization purposes
 public class PsiDie {
     static short totalNumOfRolls, numOfRolls_d100, numOfRolls_d20, numOfRolls_d12, numOfRolls_d10, numOfRolls_d8, numOfRolls_d6, numOfRolls_d4, numOfRolls_d2, pointsRolled;
@@ -9,8 +7,8 @@ public class PsiDie {
     static short[] totalRolls = new short[testRuns], totalRolls_d100 = new short[testRuns], totalRolls_d20 = new short[testRuns], totalRolls_d12  = new short[testRuns], totalRolls_d10  = new short[testRuns], totalRolls_d8 = new short[testRuns], totalRolls_d6 = new short[testRuns], totalRolls_d4 = new short[testRuns], totalRolls_d2 = new short[testRuns], points = new short[testRuns];
     static short[][] allRolls = {totalRolls_d2, totalRolls_d4, totalRolls_d6, totalRolls_d8, totalRolls_d10, totalRolls_d12, totalRolls_d20, totalRolls_d100, totalRolls, points};
     static int trimmedLength = (int) (testRuns * 0.02);
-    static byte currentFocusPoints = 1, result, psiDie, initialDieSize = 12;
-    static boolean showPoints = false, increaseDieSize = false, include_d100 = initialDieSize >= 100, 
+    static byte currentFocusPoints = 2, result, psiDie, initialDieSize = 12;
+    static boolean showPoints = false, increaseDieSize = true, include_d100 = initialDieSize >= 100, 
     include_d20 = initialDieSize >= 20, include_d12 = initialDieSize >= 12, include_d10 = initialDieSize >= 10, 
     include_d8 = initialDieSize >= 8, include_d6 = initialDieSize >= 6, include_d4 = initialDieSize >= 4;
     static boolean[] includeDice = {true, include_d4, include_d6, include_d8, include_d10, include_d12, include_d20, include_d100, true, showPoints};
@@ -29,9 +27,14 @@ public class PsiDie {
         // System.out.print(".");
         System.out.print(Math.round((System.nanoTime() - startTime) / 1_000_000)/1_000f + "s");
     }
+    /**
+     * The function `testDice()` simulates rolling all of the and keeps track of the
+     * number of rolls for each type of dice.
+     */
     public static void testDice(){
         for (int i = 0; i < testRuns; i++) {
             psiDie = initialDieSize;
+            // The code block is a do-while loop that rolls dice based on the value of `psiDie`.
             do { switch (psiDie) {
                     case 100 -> { numOfRolls_d100++; rollDice(false);
                         if (psiDie < 100) { psiDie = 20; } }
@@ -48,6 +51,8 @@ public class PsiDie {
                     case 2 -> { numOfRolls_d2++; rollDice(include_d4); }
                     default -> System.out.println(psiDie);
                 } } while (psiDie > 0);
+            // This block of code is updating the values of various variables and arrays after each
+            // round of dice rolling.
             totalRolls[i] = totalNumOfRolls;
             totalRolls_d100[i] = numOfRolls_d100;
             totalRolls_d20[i] = numOfRolls_d20;
@@ -60,7 +65,14 @@ public class PsiDie {
             points[i] = pointsRolled;
             totalNumOfRolls = pointsRolled = numOfRolls_d100 = numOfRolls_d20 = numOfRolls_d12 = numOfRolls_d10 = numOfRolls_d8 = numOfRolls_d6 = numOfRolls_d4 = numOfRolls_d2 = 0;
         }
-    };
+    }
+    /**
+     * The function "rollDice" rolls a dice and updates the points rolled, total number of rolls, and
+     * the size of the dice based on certain conditions.
+     * 
+     * @param includeHigherDie A boolean value indicating whether or not to include a higher die in the
+     * roll.
+     */
     public static void rollDice(boolean includeHigherDie){
         result = (byte) (myRand.nextInt(psiDie) + 1);
         pointsRolled += result;
@@ -68,6 +80,14 @@ public class PsiDie {
         if(result <= currentFocusPoints) psiDie -= 2;
         else if(increaseDieSize && includeHigherDie && result == psiDie) psiDie += 2;
     }
+    /**
+     * The function processes an array of short values and a name, and then prints out various
+     * statistics about the array.
+     * 
+     * @param array An array of short values representing the results of some rolls or points.
+     * @param name The "name" parameter is a String that represents the name of the rolls being
+     * processed. It can be either "Total" or "Points".
+     */
     public static void processResults(short[] array, String name){
         ArrayUtils.winsorizeArray(array);
         double arrayAverage = ArrayUtils.getArrayAverage(array);
